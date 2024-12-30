0d31b995bcf96c0a14d41b1c9cbefe01ed02d4fb
yogesh616 <ys30371@gmail.com>
2024-12-31 04:11:47 +0530

    custom mouse added

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -3,9 +3,11 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./pages/Header";
 import Footer from "./pages/Footer";
+import AnimatedCursor from "react-animated-cursor";
 const Layout = () => {
   return (
     <div>
+      <AnimatedCursor />
       <Header />
       <main>
         <Outlet />
